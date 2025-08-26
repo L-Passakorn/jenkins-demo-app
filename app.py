f6c76fb8c234a06ea380b7477c31c33065d7572c
Passakorn L. <passakornlimseng@gmail.com>
2025-08-26 17:26:18 +0700

    update: modify greeting message in app.py and ensure test_app.py is copied in Dockerfile

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello PSU Students from Jenkins!"
+    return "Hello PSU Students I'm 6510110356 from Jenkins!"
 
 
 if __name__ == "__main__":
